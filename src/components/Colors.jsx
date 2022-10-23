2670221ac303a0a5cf010726fb7732efdbdb5d33
BrianMacia <brianmacia@hotmail.com>
2022-10-23 14:40:46 -0300

    V2

---
+++ src/components/Colors.jsx
@@ -0,0 +1,19 @@
+const colors = [
+    "#845EC2",
+    "#D65DB1",
+    "#FF6F91",
+    "#FF9671",
+    "#FFC75F",
+    "#f0f06e",
+    "#F85676",
+    "#002cbd",
+    "#08d3ee",
+    "#07cc28",
+    "#06d463",
+    "#d406a7",
+    "#d40639",
+    "#d40606af",
+    "#175fa3",
+    "#c3c8cc"
+  ];
+  export default colors;
